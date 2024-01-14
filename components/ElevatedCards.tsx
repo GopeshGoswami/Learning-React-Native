9ad052a15b7745b7dd83576be8d2f3e25669be28
Gopesh Goswami <ggopesh45@gmail.com>
2024-01-15 04:59:50 +0530

    Build some flatCards and ScrollView

---
+++ components/ElevatedCards.tsx
@@ -0,0 +1,64 @@
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function ElevatedCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>ElevatedCards</Text>
+      <ScrollView horizontal={true} style={styles.container}>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>Tap</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>Me</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>To</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>Scroll</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>More</Text>
+        </View>
+        <View style={[styles.card, styles.cardElevated]}>
+          <Text style={styles.cardText}>😅</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+  },
+  container: {
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardElevated: {
+    backgroundColor: '#CAD5E2',
+    elevation: 10,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#EF5354',
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
+  },
+  cardText: {
+    color: '#000000',
+  },
+});
